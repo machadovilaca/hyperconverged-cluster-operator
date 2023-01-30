bb3e44dadf5baf302719a8296995d53f3ca03624
João Vilaça <jvilaca@redhat.com>
2023-01-30 18:00:19 +0000

    Add e2e test
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

--- tests/func-tests/monitoring_test.go
+++ tests/func-tests/monitoring_test.go
@@ -3,30 +3,33 @@ package tests_test
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	openshiftroutev1 "github.com/openshift/api/route/v1"
 	authenticationv1 "k8s.io/api/authentication/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 
-	kvtutil "kubevirt.io/kubevirt/tests/util"
-
+	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/kubevirt/tests/flags"
+	kvtutil "kubevirt.io/kubevirt/tests/util"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	openshiftroutev1 "github.com/openshift/api/route/v1"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	promApi "github.com/prometheus/client_golang/api"
 	promApiv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	promConfig "github.com/prometheus/common/config"
-	"k8s.io/client-go/kubernetes/scheme"
 
 	"github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
 	tests "github.com/kubevirt/hyperconverged-cluster-operator/tests/func-tests"
-	"kubevirt.io/client-go/kubecli"
 )
 
 var runbookClient = http.DefaultClient
@@ -50,21 +53,19 @@ var _ = Describe("[crit:high][vendor:cnv-qe@redhat.com][level:system]Monitoring"
 		prometheusRule = getPrometheusRule(virtCli)
 	})
 
-	It("Alert rules should have all the requried annotations", func() {
+	It("Alert rules should have all the required annotations", func() {
 		for _, group := range prometheusRule.Spec.Groups {
 			for _, rule := range group.Rules {
 				if rule.Alert != "" {
 					Expect(rule.Annotations).To(HaveKeyWithValue("summary", Not(BeEmpty())),
 						fmt.Sprintf("%s summary is missing or empty", rule.Alert))
-					Expect(rule.Annotations).To(HaveKeyWithValue("runbook_url", Not(BeEmpty())),
-						fmt.Sprintf("%s runbook_url is missing or empty", rule.Alert))
 					checkRunbookUrlAvailability(rule)
 				}
 			}
 		}
 	})
 
-	It("Alert rules should have all the requried labels", func() {
+	It("Alert rules should have all the required labels", func() {
 		for _, group := range prometheusRule.Spec.Groups {
 			for _, rule := range group.Rules {
 				if rule.Alert != "" {
@@ -91,13 +92,7 @@ var _ = Describe("[crit:high][vendor:cnv-qe@redhat.com][level:system]Monitoring"
 		kubevirt, err = virtCli.KubeVirt(flags.KubeVirtInstallNamespace).Update(kubevirt)
 		Expect(err).ShouldNot(HaveOccurred())
 
-		Eventually(func() *promApiv1.Alert {
-			alerts, err := promClient.Alerts(context.TODO())
-			Expect(err).ShouldNot(HaveOccurred())
-			alert := getAlertByName(alerts, "KubevirtHyperconvergedClusterOperatorCRModification")
-			return alert
-		}, 60*time.Second, time.Second).ShouldNot(BeNil())
-
+		verifyAlertExists(promClient, "KubevirtHyperconvergedClusterOperatorCRModification")
 	})
 
 	It("KubevirtHyperconvergedClusterOperatorUSModification alert should fired when there is an jsonpatch annotation to modify an operand CRs", func() {
@@ -109,15 +104,49 @@ var _ = Describe("[crit:high][vendor:cnv-qe@redhat.com][level:system]Monitoring"
 		}
 		updateHCO(virtCli, hco)
 
-		Eventually(func() *promApiv1.Alert {
-			alerts, err := promClient.Alerts(context.TODO())
-			Expect(err).ShouldNot(HaveOccurred())
-			alert := getAlertByName(alerts, "KubevirtHyperconvergedClusterOperatorUSModification")
-			return alert
-		}, 60*time.Second, time.Second).ShouldNot(BeNil())
-
+		verifyAlertExists(promClient, "KubevirtHyperconvergedClusterOperatorUSModification")
 	})
 
+	It("KubeVirtHyperconvergedClusterOperatorPodSchedulingMightBeImbalanced alert should fired when node max images is reached", func() {
+		nodes, err := virtCli.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(nodes.Items).ShouldNot(BeEmpty())
+
+		node := &nodes.Items[0]
+		maxImages, err := getNodeMaxImages(virtCli, node.Name)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		if maxImages == -1 {
+			Skip("maxImages is -1, skip test")
+		}
+
+		dhil := initDockerHubImageList("busybox", "1.")
+
+		pod := createPod(virtCli, node, dhil.getNextImage())
+		waitForPodToBeReady(virtCli, pod)
+
+		restartCount := int32(0)
+
+		nodeCurrentImages := len(node.Status.Images)
+		for i := 1; nodeCurrentImages < 100; i++ {
+			updatePodImage(virtCli, pod, dhil.getNextImage())
+
+			Eventually(func() int32 {
+				pod, err = virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+				Expect(err).ShouldNot(HaveOccurred())
+				return pod.Status.ContainerStatuses[0].RestartCount
+			}, 1*time.Minute, 5*time.Second).Should(Equal(restartCount + 1))
+			restartCount++
+
+			node, err = virtCli.CoreV1().Nodes().Get(context.TODO(), node.Name, metav1.GetOptions{})
+			Expect(err).ShouldNot(HaveOccurred())
+			nodeCurrentImages = len(node.Status.Images)
+		}
+
+		verifyAlertExists(promClient, "KubeVirtHyperconvergedClusterOperatorPodSchedulingMightBeImbalanced")
+
+		err = virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
+	})
 })
 
 func getHCO(client kubecli.KubevirtClient) v1beta1.HyperConverged {
@@ -245,4 +274,140 @@ func getPrometheusUrl(cli kubecli.KubevirtClient) string {
 	kvtutil.PanicOnError(err)
 
 	return fmt.Sprintf("https://%s", route.Spec.Host)
+}
+
+type Kubeletconfig struct {
+	Kubeletconfig struct {
+		NodeStatusMaxImages int `json:"nodeStatusMaxImages"`
+	} `json:"kubeletconfig"`
+}
+
+func getNodeMaxImages(virtCli kubecli.KubevirtClient, nodeName string) (int, error) {
+	resp, err := virtCli.CoreV1().RESTClient().Get().
+		Resource("nodes").Name(nodeName).
+		Suffix("proxy", "configz").
+		Do(context.TODO()).Raw()
+	if err != nil {
+		return 0, err
+	}
+
+	var kc Kubeletconfig
+	err = json.Unmarshal(resp, &kc)
+	if err != nil {
+		return 0, err
+	}
+
+	return kc.Kubeletconfig.NodeStatusMaxImages, nil
+}
+
+func verifyAlertExists(promClient promApiv1.API, alertName string) {
+	Eventually(func() *promApiv1.Alert {
+		alerts, err := promClient.Alerts(context.TODO())
+		Expect(err).ShouldNot(HaveOccurred())
+		alert := getAlertByName(alerts, alertName)
+		return alert
+	}, 60*time.Second, time.Second).ShouldNot(BeNil())
+}
+
+func createPod(virtCli kubecli.KubevirtClient, node *corev1.Node, imageName string) *corev1.Pod {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod",
+			Namespace: flags.KubeVirtInstallNamespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "test-container",
+					Image:   imageName,
+					Command: []string{"/bin/sh", "-c", "while [ ! -f /tmp/exit ]; do sleep 1; done"},
+				},
+			},
+			NodeName: node.Name,
+		},
+	}
+
+	_, err := virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+
+	return pod
+}
+
+func updatePodImage(virtCli kubecli.KubevirtClient, pod *corev1.Pod, imageName string) {
+	pod, err := virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+
+	pod.Spec.Containers[0].Image = imageName
+
+	_, err = virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+}
+
+func waitForPodToBeReady(virtCli kubecli.KubevirtClient, pod *corev1.Pod) {
+	Eventually(func() bool {
+		pod, err := virtCli.CoreV1().Pods(flags.KubeVirtInstallNamespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred())
+		for _, cond := range pod.Status.Conditions {
+			if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+				return true
+			}
+		}
+		return false
+	}, 60*time.Second, 1*time.Second).Should(BeTrue())
+}
+
+type DockerHubImageList struct {
+	baseImage    string
+	tagPrefix    string
+	currentIndex int
+	currentPage  int
+
+	result *struct {
+		Count   int    `json:"count"`
+		Next    string `json:"next"`
+		Results []struct {
+			Name string `json:"name"`
+		} `json:"results"`
+	}
+}
+
+func initDockerHubImageList(imageName string, tagPrefix string) *DockerHubImageList {
+	return &DockerHubImageList{
+		baseImage:    imageName,
+		tagPrefix:    tagPrefix,
+		currentIndex: 0,
+		currentPage:  0,
+		result:       nil,
+	}
+}
+
+func (dhil *DockerHubImageList) getNextImage() string {
+	if dhil.result == nil {
+		dhil.requestMoreImages(fmt.Sprintf("https://hub.docker.com/v2/repositories/library/%s/tags?name=%s", dhil.baseImage, dhil.tagPrefix))
+	}
+
+	if dhil.currentIndex >= len(dhil.result.Results) {
+		if dhil.result.Next == "" {
+			Fail("No more images")
+		}
+
+		dhil.requestMoreImages(dhil.result.Next)
+		dhil.currentIndex = 0
+	}
+
+	image := dhil.result.Results[dhil.currentIndex].Name
+	dhil.currentIndex++
+	return fmt.Sprintf("%s:%s", dhil.baseImage, image)
+}
+
+func (dhil *DockerHubImageList) requestMoreImages(target string) {
+	resp, err := http.Get(target)
+	Expect(err).ShouldNot(HaveOccurred())
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).ShouldNot(HaveOccurred())
+
+	err = json.Unmarshal(body, &dhil.result)
+	Expect(err).ShouldNot(HaveOccurred())
 }
